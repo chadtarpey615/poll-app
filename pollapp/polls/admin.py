503373b1162d2c2f169151906fb62d0fb7506627
Chad Tarpey <chadtarpey@gmail.com>
2021-05-26 20:21:11 -0500

    created template folders with a index.html file

--- pollapp/polls/admin.py
+++ pollapp/polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+admin.site.site_header = "Poll App"
+admin.site.site_title = "Poll App area"
+admin.site.site_title = "Welcome to the Poll App admin area"
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+    ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),]   
+    inline = [ChoiceInline]  
+
+
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
